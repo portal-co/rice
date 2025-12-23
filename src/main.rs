a956081bd3022e8509f99c9753b1a5f02d9fb928
Graham Kelly <gkgoat6700@gmail.com>
2025-12-23 14:04:20 -0500

    support claude-like agent processing

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,32 @@ fn main() -> std::io::Result<()> {
         .truncate(true)
         .open(file)?;
     let mut go = true;
+    let mut lx = None;
     for l in s.lines() {
         if go {
             writeln!(file, "{l}")?;
+        }
+        macro_rules! resolve {
+            ($c:expr) => {
+                match $c {
+                    c => match std::fs::File::open(c) {
+                        Ok(mut file2) => {
+                            let mut a = [0u8];
+                            while file2.read(&mut a)? == 1 {
+                                file.write_all(&a)?;
+                            }
+                        }
+                        Err(file_error) => {}
+                    },
+                }
+            };
+        }
+        if let Some(l) = l.strip_prefix("@") {
+            writeln!(file, "[[begin {l}]]")?;
+            resolve!(l.trim());
+            writeln!(file, "[[end]]")?;
+            lx = Some(l);
+            continue;
         }
         if let Some((_, b2)) = l.split_once("[[")
             && let Some((b, _)) = b2.rsplit_once("]]")
@@ -25,22 +48,15 @@ fn main() -> std::io::Result<()> {
                     go = true
                 }
                 b => match b.strip_prefix("begin ") {
-                    Some(c) if go => match std::fs::File::open(c) {
-                        Ok(mut file2) => {
-                            let mut a = [0u8];
-                            while file2.read(&mut a)? == 1 {
-                                file.write_all(&a)?;
-                            }
-                            go = false;
-                        },
-                        Err(file_error) => {
-
-                        }
-                    },
+                    Some(c) if go && lx.take().is_none_or(|a| a != c) => {
+                        resolve!(c.trim());
+                        go = false;
+                    }
                     _ => {}
                 },
             }
         }
+        lx = None;
     }
     Ok(())
 }
