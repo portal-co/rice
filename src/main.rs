5f4d6b1914bc6d2967049c327d4a0ce6d30bd324
Graham Kelly <gkgoat6700@gmail.com>
2025-12-23 14:10:21 -0500

    [AI] migrate to `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use std::io::Write;
+use clap::Parser;
+
+/// Simple CLI to run rice::splice on a file in-place.
+#[derive(Debug, Parser)]
+#[command(about = "Run rice splice on a file in-place")]
+struct Args {
+    /// File to process
+    file: std::path::PathBuf,
+}
 
 fn main() -> std::io::Result<()> {
-    let mut args = std::env::args();
-    args.next();
-    let file = args.next().unwrap();
+    let args = Args::parse();
+    let file = args.file;
     let s = std::fs::read_to_string(&file)?;
     let mut file = std::fs::OpenOptions::new()
         .write(true)
         .truncate(true)
-        .open(file)?;
+        .open(&file)?;
     rice::splice(&s, &mut file)?;
     Ok(())
 }
